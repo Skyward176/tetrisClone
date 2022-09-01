63349d6cb19a9ce469ecf2491f111e765f5b3468
Brandon Martinez <brandonam04@outlook.com>
2022-09-01 13:08:22 -0400

    Update makefile and create basic window
    Updates makefile to link SDL library and use variables for various
    paths.
    Mainfile now opens and empty window as a test of SDL funcitonality.

--- Makefile
+++ Makefile
@@ -1,4 +1,25 @@
-main: ./src/main.cpp
-	g++ ./src/main.cpp -o ./bin/main
+#Directories
+SOURCE = src
+BUILD = bin
+
+# Output
+EXEC=$(BUILD)/main.exec
+
+#Objects
+OBJ = $(BUILD)/main.o
+
+
+# SDL Compile Options
+CC_SDL =-F/Library/Frameworks -framework SDL2
+# SDL Linker Options
+LL_SDL = -F/Library/Frameworks -framework SDL2
+
+all: build
+
+$(BUILD)/main.o: $(SOURCE)/main.cpp
+	g++ -c $(CC_SDL) -o $(BUILD)/main.o $(SOURCE)/main.cpp
+
+build: $(OBJ) Makefile
+	g++ $(LL_SDL) -o $(EXEC) $(OBJ) -v
 clean:
-	rm -f ./src/main.cpp
+	rm -f $(BUILD)/*
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 #include <string>
-int main(void) {
-    std::cout<<"Hi mom!\n";
+#include <SDL2/SDL.h>
+
+void OpenNewWindow() {
+    SDL_Init (SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("My Window",0, 0, 800, 600, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+}
+
+int main() {
+    bool quit = false;
+
+    SDL_Event event;
+
+    OpenNewWindow();
+    while(!quit) {
+        SDL_WaitEvent(&event);
+
+        switch(event.type) {
+            case SDL_QUIT:
+                quit = true;
+                break;
+        }
+    }
+    SDL_Quit();
+    return 0;
 }
